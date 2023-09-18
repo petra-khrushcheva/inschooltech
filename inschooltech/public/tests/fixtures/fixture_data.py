e7fe803b3c4fc4b392cb2f62fe619e24c7a8fd64
Petra Khrushcheva <petra.khrushcheva@gmail.com>
2023-09-18 21:46:36 +0500

    fix pytest fixtures

--- inschooltech/public/tests/fixtures/fixture_data.py
+++ inschooltech/public/tests/fixtures/fixture_data.py
@@ -22,61 +22,55 @@ def assessment(lab):
     time_1 = datetime.now()
     sleep(5)
     time_2 = datetime.now()
-    labtest = Test.objects.create(
+    return Test.objects.create(
         id='217f0c27-36bb-4336-946b-64a1dfd1bc27',
         started_at=time_1,
         completed_at=time_2,
         lab_id=lab
     )
-    return labtest
 
 
 @pytest.fixture(autouse=True)
 def indicator():
-    ind_1 = Indicator.objects.create(
+    return Indicator.objects.create(
         name='способность к усвоению информации',
         id='cccdada8-de75-4974-ab58-e0233722b956'
     )
-    return ind_1
 
 
 @pytest.fixture(autouse=True)
 def metric():
-    metric_1 = Metric.objects.create(
+    return Metric.objects.create(
         name='Скорость реакции',
         unit='Секунды',
         id='815dee15-338c-4470-bdfa-53ad069bba73'
     )
-    return metric_1
 
 
 @pytest.fixture(autouse=True)
 def ind_metric(indicator, metric):
-    ind_metric_1 = IndicatorsMetric.objects.create(
+    return IndicatorsMetric.objects.create(
         indicator_id=indicator,
         metric_id=metric,
         id='74a0ec4a-12ed-4641-b506-50638f0ea650'
     )
-    return ind_metric_1
 
 
 @pytest.fixture(autouse=True)
 def reference(ind_metric):
-    ref_1 = Reference.objects.create(
+    return Reference.objects.create(
         min_score=0,
         max_score=10,
         indicator_metric_id=ind_metric,
         id='7e6f82cf-09d7-492e-84a0-464016604a02'
     )
-    return ref_1
 
 
 @pytest.fixture(autouse=True)
 def score(assessment, ind_metric):
-    score_1 = Score.objects.create(
+    return Score.objects.create(
         score=3,
         test_id=assessment,
         indicator_metric_id=ind_metric,
         id='24363974-49ed-405a-a200-d98fd08c6a9f'
     )
-    return score_1
--- inschooltech/public/tests/fixtures/fixture_user.py
+++ inschooltech/public/tests/fixtures/fixture_user.py
@@ -6,9 +6,8 @@ User = get_user_model()
 
 @pytest.fixture
 def user():
-    user_1 = User.objects.create_user(
+    return User.objects.create_user(
         email='testuser@example.com',
         username='testuser',
         password='somep4$$'
     )
-    return user_1
